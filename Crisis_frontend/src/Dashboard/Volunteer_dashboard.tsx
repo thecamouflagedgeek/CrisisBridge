d6f5d0742db114795d835378cb91d1c64db2785d
Obsidian811 <156951145+Obsidian811@users.noreply.github.com>
2025-02-03 12:53:04 +0530

    commit

--- Crisis_frontend/src/Dashboard/Volunteer_dashboard.tsx
+++ Crisis_frontend/src/Dashboard/Volunteer_dashboard.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styles from './Dashboard.module.css'
+import PieChart from './PieChart.tsx'
 
 const Dashboard: React.FC = () => 
 {
     return (
-        <div className="container">
+        <div className={styles.container}>
             <h1>Volunteer Dashboard</h1>
+            <div className={styles.glasscard}>
+                <div className={styles.piechart}>
+                    <PieChart data={[30, 20, 50]} labels={["Product A", "Product B", "Product C"]} />
+                </div>
+                <div className={styles.cardContent}>
+                    <h1>hello</h1>
+                </div>
+            </div>
         </div>
     );
 };
